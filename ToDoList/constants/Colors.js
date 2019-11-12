786b1bea3a0aa9eb5d1b4fbd1ac7e4b90fd765f5
SahilShikalgar <shikalgar32@gmail.com>
2019-11-12 14:44:09 +0530

    Removed Expo;

--- ToDoList/constants/Colors.js
+++
@@ -1,3 +0,0 @@
-export default {
-    primary: '#4B15A4'
-};
--- ToDoList/redux/app.actions.js
+++
@@ -1,5 +0,0 @@
-export const ADD_LIST_ITEM = 'ADD_LIST_ITEM';
-
-export function addListItem(payload) {
-    return { type: ADD_LIST_ITEM, payload: payload }
-}
