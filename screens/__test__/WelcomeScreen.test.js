13186453fb25505395c740af110395fa86eb1895
SahilShikalgar <shikalgar32@gmail.com>
2019-11-18 22:10:37 +0530

    Updated test casese

--- screens/__test__/WelcomeScreen.test.js
+++ screens/__test__/WelcomeScreen.test.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import WelcomeScreen from '../WelcomeScreen';
+import MainButton from '../../components/MainButton';
 
 describe('WelcomeScreen', () => {
+    const navigation = {
+        navigate: jest.fn()
+    }
     const component = shallow (
-        <WelcomeScreen />
+        <WelcomeScreen navigation={navigation} />
     );
 
-    it('should rendere properly', () => {
+    it('should render properly', () => {
         expect(component).toMatchSnapshot();
+    });
+
+    it('should call goToMainScreen on click', () => {
+        
     });
 });
